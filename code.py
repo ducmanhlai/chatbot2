ce700db9ea045fc752db2eccd30fa5f3fc3bd6ad
ducmanhlai <72874551+ducmanhlai@users.noreply.github.com>
2021-11-28 20:30:09 +0700

    Update code.py

--- code.py
+++ code.py
@@ -6,11 +6,12 @@ def update():
   b=0
   dt= requests.get('https://static.pipezero.com/covid/data.json')
   dt_json=dt.json()
+  hn=dt_json['today']['internal']["cases"]
   chet=dt_json['total']['internal']["death"]
   nhiem=dt_json['total']['internal']["cases"]
   khoi=dt_json['total']['internal']["recovered"]
   dang=nhiem-chet-khoi
-  return chet,nhiem,khoi,dang
+  return chet,nhiem,khoi,dang,hn
 def sim(ms):
     
     re="https://api.simsimi.net/v2/?text="+ms+"&lc=vn"
